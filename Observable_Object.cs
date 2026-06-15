1f038c4929c54262c9fb50d72b2507b3d3ed5c22
AkzhanZhailaubay <159834286+AkzhanZhailaubay@users.noreply.github.com>
2026-06-15 19:29:43 +0500

    Add files via upload

---
+++ Observable_Object.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Budget_Planner_Alnur_Madiyev_w68646.Core
+{
+    public class ObservableObject : INotifyPropertyChanged //call INotifyPropertyChanged EventHandler
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ Relay_Command.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Budget_Planner_Alnur_Madiyev_w68646.Core
+{
+    class RelayCommand : ICommand // call ICommand methods 
+    {
+        private readonly Action<object> execute;
+        private readonly Func<object, bool> canExecute;
+        private DependencyObject sender;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+        private bool IsValid(DependencyObject obj)
+        {
+            if (obj != null)
+            {
+                return !Validation.GetHasError(obj) && LogicalTreeHelper.GetChildren(obj).OfType<DependencyObject>().All(IsValid);
+            }
+            return true;
+
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            bool valid = IsValid(sender as DependencyObject);
+            return valid;
+        }
+
+        public void Execute(object parameter)
+        {
+            execute(parameter);
+        }
+
+    }
+
+
+
+}
